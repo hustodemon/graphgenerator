79152c7c01581bf6dd8578bfd18d48fdb12722ac
Frantisek Kobzik <fkobzik@suse.de>
2021-06-09 07:56:57 +0200

    Backend: Tidy router a bit

--- src/clj/graphgenerator/routes/home.clj
+++ src/clj/graphgenerator/routes/home.clj
@@ -6,40 +6,48 @@
    [clojure.edn :as edn]
    [graphgenerator.middleware :as middleware]
    [ring.util.response]
-   [ring.util.http-response :as response]
-   ))
+   [ring.util.http-response :as response]))
 
 
 (defn home-page [request]
   (layout/render request "home.html"))
 
 
+(defn docs [_]
+  (fn [_]
+    (-> (response/ok (-> "docs/docs.md" io/resource slurp))
+        (response/header "Content-Type" "text/plain; charset=utf-8"))))
+
+
+(defn generate-dot [req]
+  (try
+    (response/ok
+     (generator/generate-graph-dot
+      (:body-params req)))
+    (catch Throwable e
+      (response/internal-server-error (-> e .getData :msg)))))
+
+
+(defn generate [req]
+  (try
+    (response/ok
+     (generator/generate-graph-rhizome
+      (edn/read-string
+       (:body-params req))
+      ));; todo catch only exception-info
+    (catch Throwable e
+      ;; todo ISE - something else maybe
+      (response/internal-server-error (str e)))))
+
+
 (defn home-routes []
   [""
-   {:middleware [;; middleware/wrap-csrf. todo revive
-                 middleware/wrap-formats]}
+   {:middleware [middleware/wrap-formats]} ;; todo csrf when needed
    ["/" {:get home-page}]
    ["/generate-dot"
-    {:post (fn [req]
-             (try
-               (response/ok
-                (generator/generate-graph-dot
-                 (:body-params req)))
-               (catch Throwable e
-                 (response/internal-server-error (-> e .getData :msg)))))}]
+    {:post generate-dot}]
    ["/generate"
-    {:post (fn [req]
-             (try
-               (response/ok
-                (generator/generate-graph-rhizome
-                 (edn/read-string
-                  (:body-params req))
-                 ));; todo catch only exception-info
-               (catch Throwable e
-                 ;; todo ISE - something else
-                 (response/internal-server-error (str e)))))}]
+    {:post generate}]
    ["/docs"
-    {:get (fn [_]
-            (-> (response/ok (-> "docs/docs.md" io/resource slurp))
-                (response/header "Content-Type" "text/plain; charset=utf-8")))}]])
+    {:get docs}]])
 
